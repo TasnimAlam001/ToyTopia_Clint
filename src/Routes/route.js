67196b0bc579703218f827b8d358b4c65269fd47
tasnimalam001 <tasnimalam.001@gmail.com>
2023-05-21 20:59:12 +0600

    website title updatet

---
+++ src/Routes/route.js
@@ -0,0 +1,23 @@
+const currentRoute = window.location.pathname;
+
+
+if (currentRoute === '/') {
+  document.title = 'Home Page -ToyTopia';
+} 
+else if (currentRoute === '/allToys') {
+  document.title = 'ToyTopia-All Toys';
+}
+ else if (currentRoute === '/addToy') {
+  document.title = 'ToyTopia-Add Toy';
+} 
+else if (currentRoute === '/login') {
+  document.title = 'ToyTopia-Log In';
+}
+else if (currentRoute === '/myToys') {
+  document.title = 'ToyTopia-My Toys';
+}
+else if (currentRoute === '/register') {
+  document.title = 'ToyTopia-Register';
+} else {
+  document.title = 'My Website'; // fallback title
+}
